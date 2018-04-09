b89d808716419f288c36d1a91c6f23f18ab21269
Marco MOSCARDINI <marco_moscardini@hotmail.com>
2018-04-09 22:38:56 +0200

    application and crud

---
+++ src/AppBundle/Controller/MobileCallController.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use AppBundle\Entity\MobileCall;
+use DateTime;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Form;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Mobilecall controller.
+ *
+ * @Route("mobilecall")
+ */
+class MobileCallController extends Controller
+{
+    /**
+     * Provides report as indicated in specs:
+     *  Retrouver la durée totale réelle des appels effectués après le 15/02/2012 (inclus)
+     *  Retrouver le TOP 10 des volumes data facturés en dehors de la tranche horaire 
+     *  8h00-18h00, par abonné.
+     *  Retrouver la quantité totale de SMS envoyés par l'ensemble des abonnés
+     * 
+     * @Route("/summary", name="mobilecall_summary")
+     * @Method("GET")
+     */
+    public function summaryAction() {
+        $em = $this->getDoctrine()->getManager();
+
+        $totalCalls = $em->getRepository('AppBundle:MobileCall')->totalCalls(new DateTime("2012-02-15"));
+        $mobileCalls = $em->getRepository('AppBundle:MobileCall')->getTop10OffDutyCalls();
+        $totalSms = $em->getRepository('AppBundle:MobileCall')->totalSms();
+                
+        return $this->render('mobilecall/summary.html.twig', array(
+            'total_calls' => $totalCalls,
+            'mobileCalls' => $mobileCalls,
+            'total_sms' => $totalSms
+        ));
+    }
+    
+    /**
+     * Lists all mobileCall entities.
+     *
+     * @Route("/", name="mobilecall_index")
+     * @Method("GET")
+     */
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $mobileCalls = $em->getRepository('AppBundle:MobileCall')->findAll();
+        return $this->render('mobilecall/index.html.twig', array(
+            'mobileCalls' => $mobileCalls,
+        ));
+    }
+
+    /**
+     * Creates a new mobileCall entity.
+     *
+     * @Route("/new", name="mobilecall_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $mobileCall = new Mobilecall();
+        $form = $this->createForm('AppBundle\Form\MobileCallType', $mobileCall);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($mobileCall);
+            $em->flush();
+
+            return $this->redirectToRoute('mobilecall_show', array('id' => $mobileCall->getId()));
+        }
+
+        return $this->render('mobilecall/new.html.twig', array(
+            'mobileCall' => $mobileCall,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a mobileCall entity.
+     *
+     * @Route("/{id}", name="mobilecall_show")
+     * @Method("GET")
+     */
+    public function showAction(MobileCall $mobileCall)
+    {
+        $deleteForm = $this->createDeleteForm($mobileCall);
+
+        return $this->render('mobilecall/show.html.twig', array(
+            'mobileCall' => $mobileCall,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing mobileCall entity.
+     *
+     * @Route("/{id}/edit", name="mobilecall_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, MobileCall $mobileCall)
+    {
+        $deleteForm = $this->createDeleteForm($mobileCall);
+        $editForm = $this->createForm('AppBundle\Form\MobileCallType', $mobileCall);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('mobilecall_edit', array('id' => $mobileCall->getId()));
+        }
+
+        return $this->render('mobilecall/edit.html.twig', array(
+            'mobileCall' => $mobileCall,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a mobileCall entity.
+     *
+     * @Route("/{id}", name="mobilecall_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, MobileCall $mobileCall)
+    {
+        $form = $this->createDeleteForm($mobileCall);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($mobileCall);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('mobilecall_index');
+    }
+
+    /**
+     * Creates a form to delete a mobileCall entity.
+     *
+     * @param MobileCall $mobileCall The mobileCall entity
+     *
+     * @return Form The form
+     */
+    private function createDeleteForm(MobileCall $mobileCall)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('mobilecall_delete', array('id' => $mobileCall->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
+}
---
+++ src/AppBundle/Entity/MobileCall.php
@@ -0,0 +1,286 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\Index;
+
+/**
+ * MobileCall
+ *
+ * @ORM\Table(name="mobile_call")
+ * @ORM\Table(indexes={@ORM\Index(name="connection_type_idx", columns={"connection_type"})})
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\MobileCallRepository")
+ * 
+ */
+class MobileCall
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="account", type="string", length=8)
+     */
+    private $account;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="invoice", type="string", length=8)
+     */
+    private $invoice;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="reference", type="string", length=8)
+     */
+    private $reference;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="date")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at_time", type="time")
+     */
+    private $createdAtTime;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="duration_volume", type="string", length=10, nullable=true)
+     */
+    private $durationVolume;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="invoiced_duration_volume", type="string", length=10, nullable=true)
+     */
+    private $invoicedDurationVolume;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="connection_type", type="string", length=255)
+     */
+    private $connectionType;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set account
+     *
+     * @param string $account
+     *
+     * @return MobileCall
+     */
+    public function setAccount($account)
+    {
+        $this->account = $account;
+
+        return $this;
+    }
+
+    /**
+     * Get account
+     *
+     * @return string
+     */
+    public function getAccount()
+    {
+        return $this->account;
+    }
+
+    /**
+     * Set invoice
+     *
+     * @param string $invoice
+     *
+     * @return MobileCall
+     */
+    public function setInvoice($invoice)
+    {
+        $this->invoice = $invoice;
+
+        return $this;
+    }
+
+    /**
+     * Get invoice
+     *
+     * @return string
+     */
+    public function getInvoice()
+    {
+        return $this->invoice;
+    }
+
+    /**
+     * Set reference
+     *
+     * @param string $reference
+     *
+     * @return MobileCall
+     */
+    public function setReference($reference)
+    {
+        $this->reference = $reference;
+
+        return $this;
+    }
+
+    /**
+     * Get reference
+     *
+     * @return string
+     */
+    public function getReference()
+    {
+        return $this->reference;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return MobileCall
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * Set createdAtTime
+     *
+     * @param \DateTime $createdAtTime
+     *
+     * @return MobileCall
+     */
+    public function setCreatedAtTime($createdAtTime)
+    {
+        $this->createdAtTime = $createdAtTime;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAtTime
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAtTime()
+    {
+        return $this->createdAtTime;
+    }
+
+    /**
+     * Set durationVolume
+     *
+     * @param string $durationVolume
+     *
+     * @return MobileCall
+     */
+    public function setDurationVolume($durationVolume)
+    {
+        $this->durationVolume = $durationVolume;
+
+        return $this;
+    }
+
+    /**
+     * Get durationVolume
+     *
+     * @return string
+     */
+    public function getDurationVolume()
+    {
+        return $this->durationVolume;
+    }
+
+    /**
+     * Set invoicedDurationVolume
+     *
+     * @param string $invoicedDurationVolume
+     *
+     * @return MobileCall
+     */
+    public function setInvoicedDurationVolume($invoicedDurationVolume)
+    {
+        $this->invoicedDurationVolume = $invoicedDurationVolume;
+
+        return $this;
+    }
+
+    /**
+     * Get invoicedDurationVolume
+     *
+     * @return string
+     */
+    public function getInvoicedDurationVolume()
+    {
+        return $this->invoicedDurationVolume;
+    }
+
+    /**
+     * Set connectionType
+     *
+     * @param string $connectionType
+     *
+     * @return MobileCall
+     */
+    public function setConnectionType($connectionType)
+    {
+        $this->connectionType = $connectionType;
+
+        return $this;
+    }
+
+    /**
+     * Get connectionType
+     *
+     * @return string
+     */
+    public function getConnectionType()
+    {
+        return $this->connectionType;
+    }
+}
+
